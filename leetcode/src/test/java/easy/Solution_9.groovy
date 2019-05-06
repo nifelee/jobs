03af7e8fa597ddb2ede56727396ebaa1dd3403ec
nifelee <nifelee@gmail.com>
2019-05-06 12:39:52 +0900

    algorithm: leetcode Palindrome Number
    https://leetcode.com/problems/palindrome-number/

---
+++ leetcode/src/test/java/easy/Solution_9.groovy
@@ -0,0 +1,34 @@
+package easy
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+//https://leetcode.com/problems/palindrome-number/
+class Solution_9 extends Specification {
+
+  @Unroll
+  def "Palindrome Number [nums: #nums, result: #result]" () {
+    expect:
+    isPalindrome(nums) == result
+
+    where:
+    nums | result
+    121  | true
+    1221 | true
+    -121 | false
+  }
+
+  boolean isPalindrome(int x) {
+    if (x < 0 || (x % 10 == 0 && x != 0))
+      return false
+
+    int revertedNumber = 0
+    while(x > revertedNumber) {
+      revertedNumber = revertedNumber * 10 + x % 10
+      x /= 10
+    }
+
+    return x == revertedNumber || x == (revertedNumber/10) as int
+  }
+
+}
