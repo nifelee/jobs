3e1f19070dbac0b92c1065ff188acfe55771aac3
nifelee <nifelee@gmail.com>
2019-05-09 21:53:49 +0900

    algorithm: 프로그래머스 12910 문제 풀이
    https://programmers.co.kr/learn/courses/30/lessons/12910

---
+++ programmers/src/test/java/com/nifelee/lessons/Solution_12910.java
@@ -0,0 +1,35 @@
+package com.nifelee.lessons;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+//https://programmers.co.kr/learn/courses/30/lessons/12910
+@Slf4j
+public class Solution_12910 {
+
+  @Test
+  public void test() {
+    Assertions.assertThat(solution(new int[]{5, 9, 7, 10}, 5)).isEqualTo(new int[]{5, 10});
+    Assertions.assertThat(solution(new int[]{2, 36, 1, 3}, 1)).isEqualTo(new int[]{1, 2, 3, 36});
+    Assertions.assertThat(solution(new int[]{3, 2, 6}, 10)).isEqualTo(new int[]{-1});
+  }
+
+  //[5, 9, 7, 10]	5	[5, 10]
+  //[2, 36, 1, 3]	1	[1, 2, 3, 36]
+  //[3,2,6]	10	[-1]
+  public int[] solution(int[] arr, int divisor) {
+    int[] answer = Arrays.stream(arr)
+        .filter(a -> a % divisor == 0)
+        .sorted()
+        .toArray();
+
+    if (answer.length == 0)
+      answer = new int[]{-1};
+
+    return answer;
+  }
+
+}
