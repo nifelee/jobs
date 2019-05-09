1e457bd1c8c6db6da4e0dca0926e183cdc42a386
nifelee <nifelee@gmail.com>
2019-05-09 22:34:03 +0900

    algorithm: 프로그래머스 12912 문제 풀이
    https://programmers.co.kr/learn/courses/30/lessons/12912

---
+++ programmers/src/test/java/com/nifelee/lessons/Solution_12912.java
@@ -0,0 +1,26 @@
+package com.nifelee.lessons;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+//https://programmers.co.kr/learn/courses/30/lessons/12912
+@Slf4j
+public class Solution_12912 {
+
+  @Test
+  public void test() {
+    Assertions.assertThat(solution(3, 5)).isEqualTo(12L);
+    Assertions.assertThat(solution(4, 10)).isEqualTo(49L);
+    Assertions.assertThat(solution(3, 3)).isEqualTo(3L);
+    Assertions.assertThat(solution(3, -3)).isEqualTo(0L);
+    Assertions.assertThat(solution(-3, 1)).isEqualTo(-5L);
+  }
+
+  public long solution(int a, int b) {
+    long n = Math.abs(a - b) + 1;
+    // 등차수열 합
+    return n * (a + b) / 2;
+  }
+
+}
