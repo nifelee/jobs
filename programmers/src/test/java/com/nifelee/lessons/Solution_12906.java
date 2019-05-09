21ac4bcfdf5710cb65b5717b1b7eb149e5c9c380
nifelee <nifelee@gmail.com>
2019-05-09 21:40:41 +0900

    algorithm: 프로그래머스 12906 문제 풀이
    https://programmers.co.kr/learn/courses/30/lessons/12906

---
+++ programmers/src/test/java/com/nifelee/lessons/Solution_12906.java
@@ -0,0 +1,37 @@
+package com.nifelee.lessons;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//https://programmers.co.kr/learn/courses/30/lessons/12906
+@Slf4j
+public class Solution_12906 {
+
+  @Test
+  public void test() {
+    Assertions.assertThat(solution(new int[]{1,1,3,3,0,1,1})).isEqualTo(new int[]{1,3,0,1});
+  }
+
+  public int[] solution(int[] arr) {
+    List<Integer> list = new ArrayList<>();
+    int prev = -1;
+    for (int i : arr) {
+      if (i != prev)
+        list.add(i);
+
+      prev = i;
+    }
+
+    int[] answer = new int[list.size()];
+    int index = 0;
+    for (int i : list) {
+      answer[index++] = i;
+    }
+    return answer;
+  }
+
+}
